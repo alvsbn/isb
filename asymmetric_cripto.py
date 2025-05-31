6f7b7c30547f83aaf962b28ba954896c701f24b7
alvsbn <166758793+alvsbn@users.noreply.github.com>
2025-05-31 13:02:12 +0400

    add asymmetric encryption

---
+++ asymmetric_cripto.py
@@ -0,0 +1,53 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
+from cryptography.hazmat.primitives import serialization
+
+class AsymmetricEncryption:
+
+    @staticmethod
+    def generate_rsa_keys() -> tuple[RSAPrivateKey, RSAPublicKey]:
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        return private_key, private_key.public_key()
+
+    @staticmethod
+    def serialization_asymmetric_public_key(public_key: RSAPublicKey) -> bytes:
+        pem_public_key = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return pem_public_key
+
+    @staticmethod
+    def serialization_asymmetric_private_key(private_key: RSAPrivateKey) -> bytes:
+        pem_private_key = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        return pem_private_key
+
+    @staticmethod
+    def rsa_encrypt(public_key: RSAPublicKey, data: bytes) -> bytes:
+        return public_key.encrypt(
+            data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    @staticmethod
+    def rsa_decrypt(private_key: RSAPrivateKey, encrypted_data: bytes) -> bytes:
+        return private_key.decrypt(
+            encrypted_data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
